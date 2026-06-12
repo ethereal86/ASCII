0b979f0b7e07c340e7529c7695ba7b77254883d4
ethereal86 <colinyeo86@gmail.com>
2026-06-12 13:10:12 +0700

    "Hello, world!" program for build testing

---
+++ src/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello, world!" << std::endl;
+    std::cout << "This is an ASCII 3D program made by Colin!" << std::endl;
+    return 0;
+}
